25003333e4db44ed8c3307f5ea7de32313f5ebaa
Marina Rocha Maia <marinarochamaia@hotmail.com>
2018-04-13 22:33:01 -0300

    Update BuscaLocal.java

--- vrptw/src/estrategiaEvolutiva/BuscaLocal.java
+++ vrptw/src/estrategiaEvolutiva/BuscaLocal.java
@@ -16,31 +16,31 @@ public class BuscaLocal {
 		Random rnd = new Random();
 		double bl = rnd.nextDouble();
 
-		//a busca local só é feita se o fator pl for atendido
+		//a busca local sÃ³ Ã© feita se o fator pl for atendido
 		if (bl <= cBuscaLocal){
 			int count = 0;
 			while(count <= rotaClonada.listaClientes.size()/2) {	
 				count++;
 
-				//1) remove u e insere após v;
-				//2) remove u e x e insere u e x após v;
-				//3) remove u e x e insere x e u após v; (posições invertidas)
+				//1) remove u e insere apÃ³s v;
+				//2) remove u e x e insere u e x apÃ³s v;
+				//3) remove u e x e insere x e u apÃ³s v; (posiÃ§Ãµes invertidas)
 				//4) troca u e v; (SWAP)
 				//5) troca u e x com v;
 				//6) troca u e x com v e y;
-				//7) remove u e insere após v em veículos diferentes;
-				//8) remove u e x e insere u e x após v;
-				//9) remove u e x e insere x e u após v em veículos diferentes; (posições invertidas)
-				//10) troca u e v em veículos diferentes;
-				//11) troca u e x com v em veículos diferentes;
-				//12) troca u e x com v e y em veículos diferentes;
-				//13) operação 2opt
+				//7) remove u e insere apÃ³s v em veÃ­culos diferentes;
+				//8) remove u e x e insere u e x apÃ³s v;
+				//9) remove u e x e insere x e u apÃ³s v em veÃ­culos diferentes; (posiÃ§Ãµes invertidas)
+				//10) troca u e v em veÃ­culos diferentes;
+				//11) troca u e x com v em veÃ­culos diferentes;
+				//12) troca u e x com v e y em veÃ­culos diferentes;
+				//13) operaÃ§Ã£o 2opt
 
 
 				BuscaLocalRotasDiferentes blrd = new BuscaLocalRotasDiferentes();
 				BuscaLocalRotasIguais blri = new BuscaLocalRotasIguais();
 				
-				ArrayList<Integer> operacoes = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 13));
+				ArrayList<Integer> operacoes = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13));
 				Collections.shuffle(operacoes);	
 
 				for(Integer o : operacoes) {
