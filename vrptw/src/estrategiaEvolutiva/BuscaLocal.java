a0ab0df57ae643753e8e600dc679b54ee79af4a6
Marina Rocha Maia <marinarochamaia@hotmail.com>
2018-05-14 10:30:55 -0300

    Add files via upload
    Correção dos erros dos veículos que não estavam sendo percorridos corretamente

--- vrptw/src/estrategiaEvolutiva/BuscaLocal.java
+++ vrptw/src/estrategiaEvolutiva/BuscaLocal.java
@@ -10,7 +10,7 @@ import modelos.Rota;
 
 public class BuscaLocal {
 
-	public void fazBuscaLocal(int k, int n, Rota rotaClonada, double [][] matrizDeDistancias, int multa, 
+	public void fazBuscaLocal(Rota rotaClonada, double [][] matrizDeDistancias, int multa, 
 			double cBuscaLocal, Cliente deposito) throws CloneNotSupportedException{
 
 		Random rnd = new Random();
@@ -18,140 +18,143 @@ public class BuscaLocal {
 
 		//a busca local só é feita se o fator pl for atendido
 		if (bl <= cBuscaLocal){
-			int count = 0;
-			while(count <= rotaClonada.listaClientes.size()/10) {	
-				count++;
+			for(int k = 0; k < rotaClonada.getNumeroDeVeiculos(); k++) {
+				for(int n = k + 1; n < rotaClonada.getNumeroDeVeiculos(); n++) {
 
-				//1) remove u e insere após v;
-				//2) remove u e x e insere u e x após v;
-				//3) remove u e x e insere x e u após v; (posições invertidas)
-				//4) troca u e v; (SWAP)
-				//5) troca u e x com v;
-				//6) troca u e x com v e y;
-				//7) remove u e insere após v em veículos diferentes;
-				//8) remove u e x e insere u e x após v;
-				//9) remove u e x e insere x e u após v em veículos diferentes; (posições invertidas)
-				//10) troca u e v em veículos diferentes;
-				//11) troca u e x com v em veículos diferentes;
-				//12) troca u e x com v e y em veículos diferentes;
-				//13) operação 2opt
+					int count = 0;
+					while(count <= rotaClonada.listaClientes.size()/10) {	
+						count++;
+
+						//1) remove u e insere após v;
+						//2) remove u e x e insere u e x após v;
+						//3) remove u e x e insere x e u após v; (posições invertidas)
+						//4) troca u e v; (SWAP)
+						//5) troca u e x com v;
+						//6) troca u e x com v e y;
+						//7) remove u e insere após v em veículos diferentes;
+						//8) remove u e x e insere u e x após v;
+						//9) remove u e x e insere x e u após v em veículos diferentes; (posições invertidas)
+						//10) troca u e v em veículos diferentes;
+						//11) troca u e x com v em veículos diferentes;
+						//12) troca u e x com v e y em veículos diferentes;
+						//13) operação 2opt
 
 
-				BuscaLocalRotasDiferentes blrd = new BuscaLocalRotasDiferentes();
-				BuscaLocalRotasIguais blri = new BuscaLocalRotasIguais(); 
-				
-				ArrayList<Integer> operacoes = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13));
-				Collections.shuffle(operacoes);	
+						BuscaLocalRotasDiferentes blrd = new BuscaLocalRotasDiferentes();
+						BuscaLocalRotasIguais blri = new BuscaLocalRotasIguais(); 
 
-				for(Integer o : operacoes) {
+						ArrayList<Integer> operacoes = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13));
+						Collections.shuffle(operacoes);	
 
-					switch (o) {
-					
-					case 1: {
-						
-						blri.inserirApos(rotaClonada, k, matrizDeDistancias, multa, deposito);
-						
-						break;
-						
-					}
+						for(Integer o : operacoes) {
 
-					case 2: {
+							switch (o) {
 
-						blri.inserirDoisApos(rotaClonada, k, matrizDeDistancias, multa, deposito);
+							case 1: {
 
-						break;
+								blri.inserirApos(rotaClonada, k, matrizDeDistancias, multa, deposito);
 
-					}
-					
-					case 3: {
+								break;
 
-						blri.inserirDoisAposInvertido(rotaClonada, k, matrizDeDistancias, multa, deposito);
-						
-						break;
+							}
 
-					}
+							case 2: {
 
-					case 4: {
+								blri.inserirDoisApos(rotaClonada, k, matrizDeDistancias, multa, deposito);
 
-						blri.swap(rotaClonada, k, matrizDeDistancias, multa, deposito);
-						
-						break;
+								break;
 
-					}
+							}
 
-					case 5: {
+							case 3: {
 
-						blri.trocaDuasPosicoesComUmaPosicao(rotaClonada, k, matrizDeDistancias, multa, deposito);
+								blri.inserirDoisAposInvertido(rotaClonada, k, matrizDeDistancias, multa, deposito);
 
-						break;
+								break;
 
-					}
+							}
 
-					case 6: {
+							case 4: {
 
-						blri.trocaDuasPosicoesComDuasPosicoes(rotaClonada, k, matrizDeDistancias, multa, deposito);
+								blri.swap(rotaClonada, k, matrizDeDistancias, multa, deposito);
 
-						break;
+								break;
 
-					}
+							}
 
-					case 7: {
-						
-						blrd.insereApos(rotaClonada, k, n, multa, matrizDeDistancias, deposito);
+							case 5: {
 
-						break;
+								blri.trocaDuasPosicoesComUmaPosicao(rotaClonada, k, matrizDeDistancias, multa, deposito);
 
-					}
+								break;
 
-					case 8: {
+							}
 
-						blrd.insereDuasPosicoesAposUma(rotaClonada, k, n, multa, matrizDeDistancias, deposito);
-						
-						break;
+							case 6: {
 
-					}
+								blri.trocaDuasPosicoesComDuasPosicoes(rotaClonada, k, matrizDeDistancias, multa, deposito);
 
-					case 9: {
+								break;
 
-						blrd.insereDuasPosicoesAposUmaInvertido(rotaClonada, k, n, multa, matrizDeDistancias, deposito);
-						
-						break;
+							}
 
-					}
+							case 7: {
 
-					case 10: {
-						
-						blrd.trocaPosicoes(rotaClonada, k, n, multa, matrizDeDistancias, deposito);
+								blrd.insereApos(rotaClonada, k, n, multa, matrizDeDistancias, deposito);
 
-						break;
-					}
+								break;
 
-					case 11: {
+							}
 
-						blrd.trocaDuasPosicoesComUmaPosicao(rotaClonada, k, n, multa, matrizDeDistancias, deposito);
-	
-						break;
+							case 8: {
 
-					}
+								blrd.insereDuasPosicoesAposUma(rotaClonada, k, n, multa, matrizDeDistancias, deposito);
 
-					case 12: {
+								break;
 
-						blrd.trocaDuasPosicoesComDuasPosicoes(rotaClonada, k, n, multa, matrizDeDistancias, deposito);
+							}
 
-						break;
+							case 9: {
 
-					}
+								blrd.insereDuasPosicoesAposUmaInvertido(rotaClonada, k, n, multa, matrizDeDistancias, deposito);
 
-					case 13: {
+								break;
 
-						blri.doisopt(rotaClonada, k, matrizDeDistancias, multa, deposito);
+							}
 
-						break;
+							case 10: {
 
-					}
-					
+								blrd.trocaPosicoes(rotaClonada, k, n, multa, matrizDeDistancias, deposito);
 
-										
+								break;
+							}
+
+							case 11: {
+
+								blrd.trocaDuasPosicoesComUmaPosicao(rotaClonada, k, n, multa, matrizDeDistancias, deposito);
+
+								break;
+
+							}
+
+							case 12: {
+
+								blrd.trocaDuasPosicoesComDuasPosicoes(rotaClonada, k, n, multa, matrizDeDistancias, deposito);
+
+								break;
+
+							}
+
+							case 13: {
+
+								blri.doisopt(rotaClonada, k, matrizDeDistancias, multa, deposito);
+
+								break;
+
+							}
+
+							}
+						}			
 					}
 				}
 			}
--- vrptw/src/principal/Main.java
+++ vrptw/src/principal/Main.java
@@ -18,10 +18,10 @@ public class Main {
 		double menorCusto = 0; // menor custo encontrado na população inicial
 		double menorCustoDescendente = 0;// menor custo encontrado nas novas gerações
 		double menorCustoTotal = 0; // menor custo Final
-		int numeroDeRotas = 5; // mu, tamanho da população inicial
-		int gmax = 1000;// número de gerações
-		int descendentes = 25; // lamba, número de descendentes
-		int multa = 0;// multa aplicada às rotas que não chegarem dentro da janela
+		int numeroDeRotas = 1; // mu, tamanho da população inicial
+		int gmax = 5000;// número de gerações
+		int descendentes = 1; // lamba, número de descendentes
+		int multa = 1000;// multa aplicada às rotas que não chegarem dentro da janela
 		double cMutacao = 0.8; // coeficiente de mutação
 		double cBuscaLocal = 0.3; // coeficiente de busca local
 
@@ -83,41 +83,28 @@ public class Main {
 		while (geracoes < gmax) {
 			// para cada indivíduo da população (pai) 
 			for (Rota r : populacao) {
-				//os veículos utilizados são percorridos
-				for (int lv = 0; lv < r.getVeiculosUtilizados(); lv++) {
 
-					// são selecionados números aleatórios que serão utilizados para pegar os veículos
-					Random rnd = new Random();
-					int k = rnd.nextInt(r.getVeiculosUtilizados());
-					int n = rnd.nextInt(r.getVeiculosUtilizados());
+				//gerar (lambda/mu) filhos
+				for (int i = 0; i < (descendentes / numeroDeRotas); i++) {
 
-					//enquanto forem iguais, são sorteados novamente
-					while (k == n) {
-						n = rnd.nextInt(r.getVeiculosUtilizados());
-					}
+					// a rota é clonada
+					Rota rotaClonada = new Rota(clientes, veiculos, clientes.size(), multa, veiculos.size(), matrizDeDistancias);
+					rotaClonada = (Rota) r.getClone(rotaClonada);
 
-					//gerar (lambda/mu) filhos
-					for (int i = 0; i < (descendentes / numeroDeRotas); i++) {
+					//o depósito é instanciado
+					Cliente deposito = rotaClonada.getDeposito();
 
-						// a rota é clonada
-						Rota rotaClonada = new Rota(clientes, veiculos, clientes.size(), multa, veiculos.size(), matrizDeDistancias);
-						rotaClonada = (Rota) r.getClone(rotaClonada);
+					//a mutação é feita
+					Mutacao mut = new Mutacao();
+					mut.fazMutacao(rotaClonada, cMutacao, matrizDeDistancias, multa, rotaClonada.getDeposito());
 
-						//o depósito é instanciado
-						Cliente deposito = rotaClonada.getDeposito();
+					//a busca local é feita
+					BuscaLocal bl = new BuscaLocal();
+					bl.fazBuscaLocal(rotaClonada, matrizDeDistancias, multa, cBuscaLocal, deposito);
 
-						//a mutação é feita
-						Mutacao mut = new Mutacao();
-						mut.fazMutacao(rotaClonada, cMutacao, matrizDeDistancias, multa, rotaClonada.getDeposito());
+					// as novas rotas são adicionadas em um array auxiliar
+					aux.add(rotaClonada);
 
-						//a busca local é feita
-						BuscaLocal bl = new BuscaLocal();
-						bl.fazBuscaLocal(k, n, rotaClonada, matrizDeDistancias, multa, cBuscaLocal, deposito);
-
-						// as novas rotas são adicionadas em um array auxiliar
-						aux.add(rotaClonada);
-
-					}
 				}
 			}
 
@@ -138,17 +125,17 @@ public class Main {
 					menorCustoDescendente = populacao.get(i).getCustoTotalRota();
 				}
 			}
-			
+
 			//a lista auxiliar é limpa
 			aux.clear();
-			
+
 			//a melhor rota é selecionada
 			melhorRota = (Rota) populacao.get(0).getClone(melhorRota);
 
 			//é impresso o custo encontrado nessa geração
 			BigDecimal bd3 = new BigDecimal(menorCustoDescendente).setScale(2, RoundingMode.HALF_EVEN);
 			System.out.println(geracoes + " " + bd3);
-			
+
 			geracoes++;
 
 		}
@@ -156,18 +143,18 @@ public class Main {
 
 		// menor custo final é encontrado
 		if (menorCusto < menorCustoDescendente) {
-			
+
 			menorCustoTotal = menorCusto;
-			
+
 		} else {
 
 			menorCustoTotal = menorCustoDescendente;
-			
+
 		}
-		
+
 		//é impressa a melhor rota
 		for (int i = 0; i < melhorRota.getNumeroDeVeiculos(); i++) {
-			
+
 			System.out.println((i + 1) + "   " + melhorRota.listaVeiculos.get(i).ordemDeVisitacao);
 
 		}
